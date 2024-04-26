8f9c3d1ec7421ef186af19646e7367c774ef545e
minthukha <minthukha1239788@gmail.com>
2024-04-26 14:31:25 +0630

    [ Testing ] - Dumpify

---
+++ Person.cs
@@ -0,0 +1,6 @@
+﻿internal class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public object Profession { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Dumpify;
+using System;
+
 Console.WriteLine("Hello, World!");
+
+new { Name = "Dumpify", Description = "Dump any object to Console" }.Dump();
+
+var arr = new[] { 1, 2, 3, 4 }.Dump();
+
+var arr2d = new int[,] { { 1, 2 }, { 3, 4 } }.Dump();
+
+new Dictionary<string, string>
+{
+    ["Moaid"] = "Hathot",
+    ["Haneeni"] = "Shibli",
+    ["Eren"] = "Yeager",
+    ["Mikasa"] = "Ackerman",
+}.Dump();
+
+
+
+Console.ReadKey();
